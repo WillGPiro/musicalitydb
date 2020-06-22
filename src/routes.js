c15a6cccff6052e3f981ad84d01284cd3b0ea0f6
Will G Piro <willgpiro@gmail.com>
2020-06-22 15:40:53 -0700

    changes to css file

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/header.js';
+
+
+const Routes = () => (
+  <BrowserRouter>
+    <Header/>
+    
+  </BrowserRouter>
+)
+
+export default Routes;
